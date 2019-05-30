21100614bb6a58e303b6e16e5b722bba4666dc61
shiffman <daniel@shiffman.net>
2019-05-30 14:55:54 -0400

    adding image and sound classification

--- unicorn-race/sketch.js
+++ unicorn-race/sketch.js
@@ -7,7 +7,7 @@ class Player {
   }
 
   move() {
-    this.x += this.r / 4;
+    this.x += 1;
   }
 
   show() {
@@ -23,35 +23,90 @@ class Player {
 }
 const players = [];
 const images = [];
+let sky;
 
 function preload() {
   for (let i = 0; i < 4; i++) {
     images[i] = loadImage(`data/player${i}.png`);
   }
+  sky = loadImage('sky_small.png');
+}
+
+const modelJson = 'https://storage.googleapis.com/tm-speech-commands/2019-05-30-2-45-51/model.json';
+const metaDataJson =
+  'https://storage.googleapis.com/tm-speech-commands/2019-05-30-2-45-51/metadata.json';
+const recognizer = speechCommands.create('BROWSER_FFT', undefined, modelJson, metaDataJson);
+
+let video, classifier;
+
+async function listen() {
+  await recognizer.ensureModelLoaded();
+  console.log(recognizer.wordLabels());
+  recognizer.listen(
+    result => {
+      console.log(result);
+      if (result.scores[0] > 0.5) {
+        players[3].move();
+      }
+    },
+    {
+      includeSpectrogram: true,
+      probabilityThreshold: 0.75,
+      overlapFactor: 0.5 // probably want between 0.5 and 0.75. More info in README
+    }
+  );
 }
 
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(889, 500);
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  let fe = ml5.featureExtractor('MobileNet');
+  classifier = fe.classification();
+  const checkpoint =
+    'https://storage.googleapis.com/tm-pro-a6966.appspot.com/eyeo-test-3/model.json';
+
+  classifier.load(checkpoint, function() {
+    classifier.classify(video, gotResult);
+  });
+  listen();
+
   players[0] = new Player(150, images[0]);
   players[1] = new Player(250, images[1]);
   players[2] = new Player(350, images[2]);
   players[3] = new Player(450, images[3]);
 }
 
-function keyPressed() {
-  if (key == '1') {
+function gotResult(error, results) {
+  let label = results[0].label;
+  console.log(label);
+  if (label == '1') {
     players[0].move();
-  } else if (key == '2') {
+  } else if (label == '2') {
     players[1].move();
-  } else if (key == '3') {
+  } else if (label == '3') {
     players[2].move();
-  } else if (key == '4') {
-    players[3].move();
+    // } else if (label == '4') {
+    //   players[3].move();
+    // }
   }
+  classifier.classify(video, gotResult);
 }
+// function keyPressed() {
+//   if (key == '1') {
+//     players[0].move();
+//   } else if (key == '2') {
+//     players[1].move();
+//   } else if (key == '3') {
+//     players[2].move();
+//   } else if (key == '4') {
+//     players[3].move();
+//   }
+// }
 
 function draw() {
-  background(0);
+  imageMode(CORNER);
+  image(sky, 0, 0);
   for (let p of players) {
     p.show();
     if (p.win()) {
