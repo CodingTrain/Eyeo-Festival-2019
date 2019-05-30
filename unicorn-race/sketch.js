40190a42e77635943b7f23a783fb4ab331224b75
shiffman <daniel@shiffman.net>
2019-05-30 14:12:09 -0400

    adding character images

--- unicorn-race/sketch.js
+++ unicorn-race/sketch.js
@@ -1,16 +1,20 @@
 class Player {
-  constructor(y) {
+  constructor(y, img) {
     this.x = 0;
     this.y = y;
+    this.img = img;
+    this.r = 90;
   }
 
   move() {
-    this.x += 50;
+    this.x += this.r / 4;
   }
 
   show() {
-    fill(255);
-    ellipse(this.x, this.y, 50);
+    // fill(255);
+    // ellipse(this.x, this.y, this.r);
+    imageMode(CENTER);
+    image(this.img, this.x, this.y, this.r, this.r);
   }
 
   win() {
@@ -18,13 +22,20 @@ class Player {
   }
 }
 const players = [];
+const images = [];
+
+function preload() {
+  for (let i = 0; i < 4; i++) {
+    images[i] = loadImage(`data/player${i}.png`);
+  }
+}
 
 function setup() {
   createCanvas(600, 500);
-  players[0] = new Player(150);
-  players[1] = new Player(250);
-  players[2] = new Player(350);
-  players[3] = new Player(450);
+  players[0] = new Player(150, images[0]);
+  players[1] = new Player(250, images[1]);
+  players[2] = new Player(350, images[2]);
+  players[3] = new Player(450, images[3]);
 }
 
 function keyPressed() {
@@ -35,7 +46,7 @@ function keyPressed() {
   } else if (key == '3') {
     players[2].move();
   } else if (key == '4') {
-    players[2].move();
+    players[3].move();
   }
 }
 
