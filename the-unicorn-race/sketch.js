be9da457dfca53ef4cbf6cf4a94c2560eeb371ed
Daniel Shiffman <daniel@shiffman.net>
2019-06-03 18:14:07 -0500

    refactor a bit

--- the-unicorn-race/sketch.js
+++ the-unicorn-race/sketch.js
@@ -39,6 +39,22 @@ function draw() {
   }
 }
 
+function gotImageResults(error, results) {
+  if (results) {
+    const label = results[0].label;
+    players[label].move(5);
+  }
+  imageClassifier.classify(video, gotImageResults);
+}
+
+function gotSoundResults(err, results) {
+  if (results) {
+    if (results[0].label == 'audience') {
+      players[3].move(100);
+    }
+  }
+}
+
 function keyPressed() {
   if (key == '1') {
     players[0].move();
